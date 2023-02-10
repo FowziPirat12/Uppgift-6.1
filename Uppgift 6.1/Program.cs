ca492ff8676367f6da55dcc00c35fc292a84d878
Caspian2 <111962656+Dungeater1337@users.noreply.github.com>
2023-02-10 10:18:24 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Övning_6._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in tre olika tal:");
+            int tal1 = int.Parse(Console.ReadLine());
+            
+            int tal2 = int.Parse(Console.ReadLine());
+            int tal3 = int.Parse(Console.ReadLine());
+            Addera(tal1, tal2, tal3);
+            
+        }
+
+        static void Addera(int tal1, int tal2, int tal3)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Summan av talen blir:");
+            Console.WriteLine(tal1 + tal2 + tal3);
+        }
+    }
+}
